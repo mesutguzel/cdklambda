9788a16a2cbc30c60442c67bbeabe3857b70ddbf
Mesut Doganguzel <mesutguzel3501@gmail.com>
2022-02-08 03:50:05 -0500

    Initial commit

---
+++ test/cdklambda.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Cdklambda = require('../lib/cdklambda-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdklambda-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Cdklambda.CdklambdaStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
